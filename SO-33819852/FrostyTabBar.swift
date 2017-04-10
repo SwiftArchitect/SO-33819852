45927c061ad31b471edc9755ce4e110bed831887
Xavier Schott <assembla@thegothicparty.com>
2017-04-09 22:02:38 -0700

    removed superfluous self

--- SO-33819852/FrostyTabBar.swift
+++ SO-33819852/FrostyTabBar.swift
@@ -29,8 +29,8 @@ class FrostyTabBar: UITabBar {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         let frost = UIVisualEffectView(effect: UIBlurEffect(style: .light))
-        frost.frame = self.bounds
+        frost.frame = bounds
         frost.autoresizingMask = .flexibleWidth
-        self.insertSubview(frost, at: 0)
+        insertSubview(frost, at: 0)
     }
 }
