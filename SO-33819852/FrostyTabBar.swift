b0db3c86ff9fe3dd5a9156801b0d40087a32808d
Xavier Schott <assembla@thegothicparty.com>
2018-02-03 20:26:39 -0800

    Build lastest iOS (11)

--- SO-33819852/FrostyTabBar.swift
+++ SO-33819852/FrostyTabBar.swift
@@ -2,7 +2,7 @@
 //  FrostyTabBar.swift
 //  SO-33819852
 //
-//  Copyright © 2017 Xavier Schott
+//  Copyright © 2017, 2018 Xavier Schott
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
